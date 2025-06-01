ce183f5b76972d07c21a0e2a35948c3057af301d
Nguyenthutrang22174600114 <118276760+Nguyenthutrang22174600114@users.noreply.github.com>
2025-06-01 12:22:31 +0700

    Delete mfa_demo.py

--- mfa_demo.py
+++
@@ -1,4 +0,0 @@
-Python 3.6.8 (v3.6.8:3c6b436a57, Dec 24 2018, 02:10:22) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
